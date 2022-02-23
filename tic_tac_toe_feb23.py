c45b37e2c5411e87c7ec9fa93d63549c029af72b
Xander Hunt <hunt.xander@gmail.com>
2022-02-23 12:29:55 -0700

    Assignment name added in comment

--- tic_tac_toe_feb23.py
+++ tic_tac_toe_feb23.py
@@ -2,6 +2,7 @@
 File: tic_tac_toe_feb23.py
 Author: Xander Hunt
 Purpose: Create a game of tic-tac-toe to prove knowledge of Python fundamentals and understanding of git and github
+CSE 210 01 Prove
 """
 
 
